ffd39022b16529c399003f3773478d5c41847315
Dan Carley <dan.carley@gmail.com>
2019-12-07 15:34:02 +0000

    2019.day01: Part2 solution
    TIL:
    
    - `let` for evaluating fuel before the conditional
    - `if-not` instead of `(if (not))`
    - `pos?` instead of `(> 0)`
    - first use of `loop` and `recur` outside the Brave book

--- src/adventofcode/2019/day01.clj
+++ src/adventofcode/2019/day01.clj
@@ -22,3 +22,22 @@
   (->> fixture
        (map fuel)
        (reduce +)))
+
+(defn total-fuel
+  "Fuel required to launch a mass and its fuel, etc.
+  So, for each module mass, calculate its fuel and add it to the total. Then,
+  treat the fuel amount you just calculated as the input mass and repeat the
+  process, continuing until a fuel requirement is zero or negative."
+  [mass]
+  (loop [extra-mass mass
+         total      0]
+    (let [extra-fuel (fuel extra-mass)]
+      (if-not (pos? extra-fuel)
+        total
+        (recur extra-fuel (+ total extra-fuel))))))
+
+(defn part2
+  []
+  (->> fixture
+       (map total-fuel)
+       (reduce +)))
--- test/adventofcode/2019/day01_test.clj
+++ test/adventofcode/2019/day01_test.clj
@@ -10,4 +10,12 @@
     (is (= 33583 (day01/fuel 100756))))
 
   (testing "part1 solution"
-    (is (= 3373568 (day01/part1)))))
+    (is (= 3373568 (day01/part1))))
+
+  (testing "part2 examples"
+    (is (= 2 (day01/total-fuel 14)))
+    (is (= 966 (day01/total-fuel 1969)))
+    (is (= 50346 (day01/total-fuel 100756))))
+
+  (testing "part2 solution"
+    (is (= 5057481 (day01/part2)))))
