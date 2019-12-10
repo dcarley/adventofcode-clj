086dfc8af0e13b95821f49bd91c9017dd25d9cac
Dan Carley <dan.carley@gmail.com>
2019-12-10 21:56:22 +0000

    2019.day02: Update language.

--- src/adventofcode/2019/day02.clj
+++ src/adventofcode/2019/day02.clj
@@ -19,41 +19,41 @@
    2 (* x y)))
 
 (defn deref-value
-  "Deference a value from an index stored in at another index."
-  [intcodes index]
-  (nth intcodes (nth intcodes index)))
+  "Deference a value from an address stored in at another address."
+  [intcodes address]
+  (nth intcodes (nth intcodes address)))
 
-(defn step
-  "Execute one step/pass of the program."
-  [intcodes index]
-  (let [opcode (nth intcodes index)
-        x      (deref-value intcodes (+ index 1))
-        y      (deref-value intcodes (+ index 2))
-        out    (nth intcodes (+ index 3))]
+(defn instruction
+  "Execute one instruction of the program."
+  [intcodes address]
+  (let [opcode (nth intcodes address)
+        x      (deref-value intcodes (+ address 1))
+        y      (deref-value intcodes (+ address 2))
+        out    (nth intcodes (+ address 3))]
     (assoc intcodes out (op opcode x y))))
 
 (defn execute
-  "Keep executing steps until we encounter an opcode greater than 2."
-  [program]
-  (loop [intcodes program
-         index    0]
-    (if (> (nth intcodes index) 2)
+  "Keep executing instructions until we encounter an opcode greater than 2."
+  [memory]
+  (loop [intcodes memory
+         address  0]
+    (if (> (nth intcodes address) 2)
       intcodes
-      (recur (step intcodes index) (+ index 4)))))
+      (recur (instruction intcodes address) (+ address 4)))))
 
 (defn restore-state
+  [intcodes noun verb]
+  (assoc intcodes
+         1 noun
+         2 verb))
   "Restore the gravity assist program (your puzzle input) to the '1202 program
   alarm' state it had just before the last computer caught fire. To do this,
   before running the program, replace position 1 with the value 12 and replace
   position 2 with the value 2."
-  [intcodes]
-  (assoc intcodes
-         1 12
-         2 2))
 
 (defn part1
   []
   (-> fixture
-      restore-state
+      (restore-state 12 2)
       execute
       first))
