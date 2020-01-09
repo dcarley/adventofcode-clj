abf2cf16ee2d159e8e5fee40bbe5081a034371d0
Dan Carley <dan.carley@gmail.com>
2020-01-09 11:42:27 +0000

    2019.day06: Part2 solution
    Refactored part1 to return all parents since there's not much (10ms)
    difference is collecting them all and then counting.

--- src/adventofcode/2019/day06.clj
+++ src/adventofcode/2019/day06.clj
@@ -1,6 +1,7 @@
 (ns adventofcode.2019.day06
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def fixture
   (-> "2019/day06"
@@ -19,18 +20,30 @@
        (map orbit-entry)
        (into (sorted-map))))
 
-(defn count-parents
+(defn orbit-parents
   [orbits planet]
-  (loop [parent planet
-         count 0]
-    (if (= parent "COM")
-      count
-      (recur (get orbits parent) (inc count)))))
+  (->> planet
+       (iterate (partial get orbits))
+       (take-while some?)
+       rest))
+
+(defn orbit-transfers
+  [orbits from to]
+  (let [from-parents (set (orbit-parents orbits from))
+        to-parents (set (orbit-parents orbits to))]
+    (into (set/difference from-parents to-parents)
+          (set/difference to-parents from-parents))))
 
 (defn part1
   []
   (let [orbits (orbit-map fixture)]
     (->> orbits
          keys
-         (map (partial count-parents orbits))
+         (map (partial orbit-parents orbits))
+         (map count)
          (reduce +))))
+
+(defn part2
+  []
+  (let [orbits (orbit-map fixture)]
+    (count (orbit-transfers orbits "YOU" "SAN"))))
--- test/adventofcode/2019/day06_test.clj
+++ test/adventofcode/2019/day06_test.clj
@@ -3,7 +3,7 @@
             [adventofcode.2019.day06 :as day06]
             [clojure.string :as str]))
 
-(def example
+(def example-input1
   (str/split-lines "COM)B
 B)C
 C)D
@@ -15,6 +15,21 @@ D)I
 E)J
 J)K
 K)L"))
+
+(def example-input2
+  (str/split-lines "COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN"))
 
 (deftest day06
   (testing "orbit-map"
@@ -29,16 +44,33 @@ K)L"))
             "J" "E"
             "K" "J"
             "L" "K"}
-           (day06/orbit-map example))))
+           (day06/orbit-map example-input1))))
 
-  (testing "count-parents"
-    ;; D directly orbits C and indirectly orbits B and COM, a total of 3 orbits.
-    (is (= 3 (day06/count-parents (day06/orbit-map example) "D")))
-    ;; L directly orbits K and indirectly orbits J, E, D, C, B, and COM, a total of 7 orbits.
-    (is (= 7 (day06/count-parents (day06/orbit-map example) "L")))
-    ;; COM orbits nothing.
-    (is (= 0 (day06/count-parents (day06/orbit-map example) "COM"))))
+  (testing "part1 examples"
+    (let [example (day06/orbit-map example-input1)]
+      ;; D directly orbits C and indirectly orbits B and COM, a total of 3 orbits.
+      (let [parents (day06/orbit-parents example "D")]
+        (is (= '("C" "B" "COM") parents))
+        (is (= 3 (count parents)))
+
+      ;; L directly orbits K and indirectly orbits J, E, D, C, B, and COM, a total of 7 orbits.
+      (let [parents (day06/orbit-parents example "L")]
+        (is (= '("K" "J" "E" "D" "C" "B" "COM") parents))
+        (is (= 7 (count parents)))
+
+      ;; COM orbits nothing.
+      (let [parents (day06/orbit-parents example "COM")]
+        (is (= '() parents))
+        (is (= 0 (count parents))))))))
 
   (testing "part1 solution"
-    (is (= 160040 (day06/part1)))))
+    (is (= 160040 (day06/part1))))
 
+  (testing "part2 examples"
+    (let [example (day06/orbit-map example-input2)]
+      (let [transfers (day06/orbit-transfers example "YOU" "SAN")]
+        (is (= #{"K" "J" "E" "I"} transfers))
+        (is (= 4 (count transfers))))))
+
+  (testing "part2 solution"
+    (is (= 373 (day06/part2)))))
